60f7d283eacab5b46236eeadc8fd37e50d104197
nepack2002 <nepack2002@gmail.com>
2024-05-29 09:07:27 +0700

    “new”

--- app/Console/Commands/Date.php
+++ app/Console/Commands/Date.php
@@ -27,15 +27,12 @@ class Date extends Command
      */
     public function handle()
     {
-        // Get the current datetime
         $now = Carbon::now();
 
-        // Find all schedules with a datetime less than the current datetime and status not equal to 2
         $expiredSchedules = Schedule::where('datetime', '<', $now)
             ->where('status', '!=', '2')
             ->get();
 
-        // Update the status of each expired schedule to 2
         foreach ($expiredSchedules as $schedule) {
             $schedule->status = '2';
             $schedule->save();
